cf338988350bc3d7827c9ed7ad7362d0022cb53f
Shero Shvan <sheroshvan@outlook.dk>
2025-08-29 00:09:55 +0200

    Add simple calculator project with unit tests
    - Added Calculator class with basic arithmetic functions
    - Created MSTest unit test project

---
+++ Lesson01/Simple_Calculator/Calculator_test/Calculator_test.cs
@@ -0,0 +1,30 @@
+﻿using Simple_Calculator_ex1;
+
+namespace Calculator_test
+{
+    [TestClass]
+    public class Calculator_test
+    {
+        private Calculator _calculator;
+        [TestInitialize]
+        public void Setup()
+        {
+            // Code that runs before each test
+            _calculator = new Calculator();
+
+        }
+        [TestMethod]
+        public void Test_Sum()
+        {
+            // Arrange
+            int a = 5;
+            int b = 3;
+            int expected = 8;
+            // Act
+            int result = _calculator.Add(a, b);
+            // Assert
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
