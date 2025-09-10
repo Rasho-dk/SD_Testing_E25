ddc8098a8fe8b8895af00eb1bc8ca01fb82a6de9
Shero Shvan <sheroshvan@outlook.dk>
2025-09-10 15:34:27 +0200

    Add MoqCustomer project with repository and tests
    Introduces the MoqCustomer solution containing a customer repository class, interface, and service. Adds unit tests using Moq and NUnit to verify customer retrieval and not-found scenarios.

---
+++ Lesson03/MoqCustomer/MoqCustomer/MoqCustomerRepo.cs
@@ -0,0 +1,25 @@
+﻿namespace MoqCustomer
+{
+    public class MoqCustomerRepo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+
+    public interface ICustomerRepository
+    {
+        MoqCustomerRepo GetCustomerById(int id);
+    }
+
+    public class CustomerService(ICustomerRepository customerRepository)
+    {
+        private readonly ICustomerRepository _customerRepository = customerRepository;
+
+        public MoqCustomerRepo GetCustomer(int id)
+        {
+            return _customerRepository.GetCustomerById(id) ?? throw new ArgumentNullException(null, $"Customer with given id = {id} not found");
+        }
+    }
+}
---
+++ Lesson03/MoqCustomer/Test/MoqCustomerTest.cs
@@ -0,0 +1,58 @@
+﻿using Moq;
+using MoqCustomer;
+
+namespace Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        private MoqCustomerRepo? _testCustomer;
+        private Mock<ICustomerRepository> _mock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mock = new Mock<ICustomerRepository>();
+
+            _testCustomer = new MoqCustomerRepo
+            {
+                Id = 1,
+                Name = "John Doe",
+                Email = "",
+                PhoneNumber = "123-456-7890",
+            };
+
+        }
+
+        [Test]
+        public void GetCustomer()
+        {
+            //arrange
+            const int customerId = 1;
+
+            _mock.Setup(repo => repo.GetCustomerById(customerId)).Returns(_testCustomer!);
+            var service = new CustomerService(_mock.Object);
+            //act
+            var currentCustomer = service.GetCustomer(customerId);
+            //assert
+            Assert.That(currentCustomer, Is.EqualTo(_testCustomer));
+        }
+        [Test]
+        public void GetCustomer_NotFound()
+        {
+            //arrange
+            const int customerId = 1;
+            const int customerNotFoundId = 2;
+
+            _mock.Setup(repo => repo.GetCustomerById(customerId)).Returns(_testCustomer!);
+            var service = new CustomerService(_mock.Object);
+
+            //act & assert
+            Assert.That(() => service.GetCustomer(customerNotFoundId),
+                Throws.TypeOf<ArgumentNullException>()
+                    .With.Message
+                    .EqualTo($"Customer with given id = {customerNotFoundId} not found"));
+
+        }
+    }
+}
