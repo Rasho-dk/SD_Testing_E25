fdd31801a6e8fd32c2e7fdcbe957199fe85ec38b
Shero Shvan <sheroshvan@outlook.dk>
2025-09-29 23:14:29 +0200

    Add integration tests for GradesSQL and CurrencyApi
    Added `GradesTest` to test the `GradesSQL` service, including a
    setup method and a test to verify the `Get` method returns
    non-empty results.
    
    Added `TestCurrency` to test the `CurrencyApi` service, including
    a test for retrieving exchange rates from a real API using
    environment variables. Assertions ensure the result is valid,
    the currency code matches, and the exchange rate is positive.

---
+++ Lesson05/Solutions/SD_MeasureConverter/TestMeasureConverterLib/integrationTests/GradesTest.cs
@@ -0,0 +1,26 @@
+﻿using MeasureConverterLib.Services;
+
+namespace TestMeasureConverterLib.integrationTests
+{
+    public class GradesTest
+    {
+        private GradesSQL _gradesSql;
+        [SetUp]
+        public void Setup()
+        {
+            _gradesSql = new GradesSQL();
+        }
+
+        [Test]
+        public async Task TestGetGrades()
+        {
+            var grades = await GradesSQL.Get();
+            Assert.That(grades.Count, Is.GreaterThan(0));
+
+            foreach (var grade in grades)
+            {
+            }
+        }
+
+    }
+}
---
+++ Lesson05/Solutions/SD_MeasureConverter/TestMeasureConverterLib/integrationTests/TestCurrency.cs
@@ -0,0 +1,52 @@
+﻿using DotNetEnv;
+using MeasureConverterLib.Models;
+using MeasureConverterLib.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMeasureConverterLib.integrationTests
+{
+    [TestFixture]
+    [Category("IntegrationTest")]
+    public class TestCurrency
+    {
+
+        #region Integration test
+        [Test]
+        public async Task GetCurrencyAsync_ShouldCallRealApi()
+        {
+            Env.TraversePath().Load();
+            var currencyApi = new CurrencyApi(new HttpClient(),
+                Environment.GetEnvironmentVariable("BASEURL")!,
+                Environment.GetEnvironmentVariable("APIKEY")!);
+            var result = await currencyApi.GetExchangeRatesAsync(baseCurrency: "DKK", targetCurrency: "USD");
+
+            //assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result?.Code, Is.EqualTo("USD"));
+                Assert.That(result?.Value, Is.GreaterThan(0));
+            });
+
+        }
+
+        //[Test]
+        //[TestCase]
+        //public void Convert_ValidInput_ReturnsExpectedResult()
+        //{
+        //    //arrange
+        //    _currency = new Currency("USD");
+        //    //act
+        //    var result = _currency.Convert(100);
+        //    //assert
+        //    Assert.That(result, Is.EqualTo(15.69));
+
+        //}
+
+        #endregion
+    }
+}
